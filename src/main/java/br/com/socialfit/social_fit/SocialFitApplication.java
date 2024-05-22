30b34fcec9fa40449acb06da494e571a4fb7c79d
Lucas Junker Narais <lucasnarvais@gmail.com>
2024-05-22 00:54:22 -0300

    adicionando config do cors

--- src/main/java/br/com/socialfit/social_fit/SocialFitApplication.java
+++ src/main/java/br/com/socialfit/social_fit/SocialFitApplication.java
@@ -1,13 +1,16 @@
 package br.com.socialfit.social_fit;
 
+import br.com.socialfit.social_fit.config.WebConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan({"br.com.socialfit.social_fit.repositories", "br.com.socialfit.social_fit.controllers", "br.com.socialfit.social_fit.service"})
 @EnableJpaRepositories(basePackages = "br.com.socialfit.social_fit.repositories")
+@Import(WebConfig.class)
 public class SocialFitApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SocialFitApplication.class, args);
